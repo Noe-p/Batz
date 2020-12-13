fb2b2774e138c5d2e5246dd9ba3605df2b9d73ed
Noé <noephilippe29@gmail.com>
2020-12-13 18:22:22 +0100

    1ere itération du carrousel

--- main.js
+++ main.js
@@ -62,3 +62,47 @@ window.addEventListener('scroll', () =>{
       nav.classList.remove('positionFin');
    }
 });
+
+//CARROUSEL
+$(document).ready(function() {
+
+   var $carrousel = $('#photos'), // on cible le bloc du carrousel
+      $img = $('#photos img'), // on cible les images contenues dans le carrousel
+      indexImg = $img.length - 1, // on définit l'index du dernier élément
+      i = 0, // on initialise un compteur
+      $currentImg = $img.eq(i); // enfin, on cible l'image courante, qui possède l'index i (0 pour l'instant)
+
+   $img.css('display', 'none'); // on cache les images
+   $currentImg.css('display', 'block'); // on affiche seulement l'image courante
+
+   $carrousel.append('<img class="prev" src="photos/flecheGauche.png"/> <img class="next" src="photos/flecheDroite.png"/>');
+
+   $('.next').click(function() { // image suivante
+
+      i++; // on incrémente le compteur
+
+      if (i <= indexImg) {
+         $img.css('display', 'none'); // on cache les images
+         $currentImg = $img.eq(i); // on définit la nouvelle image
+         $currentImg.css('display', 'block'); // puis on l'affiche
+      } else {
+         i = indexImg;
+      }
+
+   });
+
+   $('.prev').click(function() { // image précédente
+
+      i--; // on décrémente le compteur, puis on réalise la même chose que pour la fonction "suivante"
+
+      if (i >= 0) {
+         $img.css('display', 'none');
+         $currentImg = $img.eq(i);
+         $currentImg.css('display', 'block');
+      } else {
+         i = 0;
+      }
+
+   });
+
+});
